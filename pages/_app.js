442a7cc5ddf003cdc00842775226a9a7633d0343
ferfachin <fernandofachinrm@gmail.com>
2023-03-11 11:30:14 -0400

    Add components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '/styles/globals.css';
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
+
+const Myapp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/4122f8b55b.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default Myapp;
