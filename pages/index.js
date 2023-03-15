0b8fbac2b23905566226a70d852ccb0c7d6549dc
ferfachin <fernandofachinrm@gmail.com>
2023-03-15 13:02:28 -0400

    banner

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import { Banner } from '../components';
 
 const index = () => (
-  <div>index</div>
+  <div className="flex justify-center sm:px-4 p-12">
+    <div className="w-full minmd:w-4/5">
+      <Banner
+        name="Discover, collect, and sell extraordinary NFTs"
+        childStyles="md:text-4xl sm:text-2xl xs=text-xl text-left"
+        parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:4-55 rounded-3xl"
+      />
+    </div>
+  </div>
 );
 
 export default index;
